3ee0ad1a7539a747e006640952d93eaa0059cdc7
Elena <elenaharan@gmail.com>
2021-12-15 23:49:14 +0000

    Implemented genre, director and synopsis dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
+import { GenreCardComponent } from './genre-card/genre-card.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -33,7 +36,10 @@ const appRoutes: Routes = [
     UserRegistrationFormComponent,
     UserLoginFormComponent,
     MovieCardComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    DirectorViewComponent,
+    SynopsisComponent,
+    GenreCardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DirectorViewComponent } from '../director-view/director-view.component';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { SynopsisComponent } from '../synopsis/synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -10,11 +15,19 @@ export class MovieCardComponent implements OnInit {
   //a variable names movies is declared as an array.
   //This is where the movies from the API call will be kept.
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  user: any = localStorage.getItem('user');
+  favouriteMovies: any[] = this.user.FavouriteMovies;
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    ) { }
 
   //the function is called inside lifecycle hook ngOnInit()
   ngOnInit(): void {
     this.getMovies();
+    //this.getUserFavourites();
   }
 
   getMovies(): void {
@@ -24,4 +37,83 @@ export class MovieCardComponent implements OnInit {
       return this.movies;
     });
   }
+
+  openDirectorDialog(
+    name: string,
+    bio: string
+  ): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: {
+        name,
+        bio,
+      },
+      width: '500px',
+    });
+  }
+
+  openSynopsisDialog(
+    title: string,
+    description: string,
+  ): void {
+    this.dialog.open(SynopsisComponent, {
+      data: {
+        title,
+        description,
+      },
+      width: '500px'
+    });
+  }
+
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreCardComponent, {
+      data: { name, description },
+      width: '500px',
+    });
+  }
+
+  /*getUserFavourites(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getFavourites(this.user.Username).subscribe((result: any) => {
+      this.favouriteMovies = result.Favourites;
+      return this.favouriteMovies;
+    });
+  }
+
+  addToFavourites(movieId: string, title: string): void {
+    this.fetchApiData
+    .AddToFavourites(this.user.Username, movieId)
+    .subscribe((result: any) => {
+      this.snackBar.open(
+        `${title} has been added to your favourite movies!`, 'Amazing!', {
+          duration: 3000,
+        }
+      );
+      this.ngOnInit();
+    });
+    return this.getUserFavourites();
+  }
+
+  deleteFromFavourites(movieId: string, title: string): void {
+    this.fetchApiData
+      .deleteFromFavourites(this.user.Username, movieId)
+      .subscribe((result: any) => {
+        this.snackBar.open(
+          `${title} has been removed from your favourite movies`, 'Ok!', {
+            duration: 3000,
+          }
+        );
+        this.ngOnInit();
+      });
+    return this.getUserFavourites();
+  }
+
+  isFavourite(movieId: string): boolean {
+    return this.favouriteMovies.some((movie) => movie._id === movieId);
+  }
+
+  toggleFavourites(movie: any): void {
+    this.isFavourite(movie._id)
+   // ? this.deleteFromFavourites(movie._id, movie.Title)
+   // : this.addToFavourites(movie._id, movie.Title);
+  }*/
 }
